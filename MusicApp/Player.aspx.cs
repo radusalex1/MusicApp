bbcc08cc333eabe11312a9197daba742f18f7961
radus_alexandru@yahoo.com <radus_alexandru@yahoo.com>
2021-08-18 15:07:26 +0300

    auto get ids from dbo when no playlist available

--- MusicApp/Player.aspx.cs
+++ MusicApp/Player.aspx.cs
@@ -8,18 +8,18 @@ public partial class _Default : System.Web.UI.Page
 {
     string ConnectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
 
-    public void FetchIdsFromDboHelper(List<string> list )
+    public void FetchIdsFromDboHelper(List<string> list)
     {
         var con = new SqlConnection(ConnectionString);
         con.Open();
-        
-            var cmd = new SqlCommand("select top(10) YoutubeId from SongsMainTable where SentToPlayPart = 0;", con);
-            SqlDataReader result = cmd.ExecuteReader();
-            while (result.Read())
-            {
-                list.Add(result[0].ToString());
-            }
-          
+
+        var cmd = new SqlCommand("select top(10) YoutubeId from SongsMainTable where SentToPlayPart = 0;", con);
+        SqlDataReader result = cmd.ExecuteReader();
+        while (result.Read())
+        {
+            list.Add(result[0].ToString());
+        }
+
         con.Close();
     }
 
@@ -27,18 +27,22 @@ public partial class _Default : System.Web.UI.Page
     {
         List<string> list = new List<string>();
 
-        FetchIdsFromDboHelper(list);
+        while (list.Count == 0)
+        {
+            FetchIdsFromDboHelper(list);
+        }
+
         string serializedList = (new JavaScriptSerializer()).Serialize(list);
         ListOfIds.Value += serializedList;
-
         updateSongs();
+
     }
 
     public void updateSongs()
     {
         var con = new SqlConnection(ConnectionString);
         con.Open();
-        var cmd_update = new SqlCommand("update SongsMainTable set SentToPlayPart = 1  where SentToPlayPart=0",con);
+        var cmd_update = new SqlCommand("update SongsMainTable set SentToPlayPart = 1  where SentToPlayPart=0", con);
         cmd_update.ExecuteNonQuery();
         con.Close();
     }
